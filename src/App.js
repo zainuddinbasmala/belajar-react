ed71754bebd9b2bb31f3a04b6bc5621160625918
zainuddinbasmala <zainuddinbasmala@gmail.com>
2022-03-19 08:50:19 +0700

    edit product

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Menu from './components/Menu';
+import ProductCard from './components/ProductCard';
 import products from './Services/data/product';
 
 function App() {
@@ -162,23 +163,7 @@ function App() {
 
       {products.map((item, index) => {
         const priceAfterDiscount = item.originalPrice - (item.originalPrice * item.discountPercent) /100;
-        return (
-          <div className="m-2 p-2 border-2 rounded-lg w-64">
-            {item.discountPercent > 0 && (
-          <p className="absolute top-2 left-2 bg-white text-red px-2 py-0.5 rounded-lg text-sm">- {item.discountPercent}%</p>)}
-          <div className="relative">
-          <img src={`images/${item.image}`} className="" alt={item.name}/>
-          {item.discountPercent > 0 && (<p className="absolute top-2 left-2 bg-white text-red px-2 py-0.5 rounded-lg text-sm">- {item.discountPercent}%</p>)}</div>
-          <h3 className="font-semibold">{item.name}</h3>
-          <p>{item.description}</p>
-          {item.discountPercent > 0 && (
-          <p className="text-xs line-through">Rp. {item.originalPrice}</p>)}
-          <div className="flex flex-wrap justify-between">
-          <p className="font-semibold">Rp. {priceAfterDiscount}</p>
-          <button className="flex-end px-3 py-2 bg-orange-600 text-white rounded-full text-sm">BUY NOW</button>
-          </div>
-          </div>
-        )
+        return <ProductCard item={item} />
       })}
 
     </div>
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,24 @@
+import CurrencyFormat from 'react-currency-format';
+const ProductCard = (props) => {
+    const item = props.item;
+    const priceAfterDiscount = item.originalPrice - (item.originalPrice * item.discountPercent) /100;
+    return <div className="m-2 p-2 border-2 rounded-lg w-64">
+    <div className="relative">
+     <img src={`images/${props.item.image}`} className="" alt={props.item.name}/>
+     {props.item.discountPercent > 0 && (<p className="absolute top-2 left-2 bg-white text-red px-2 py-0.5 rounded-lg text-sm">- {item.discountPercent}%</p>)}</div>
+     <h3 className="font-semibold">{props.item.name}</h3>
+     <p>{props.item.description}</p>
+     {props.item.discountPercent > 0 && (
+     <p className="text-xs line-through">Rp. {props.item.originalPrice}</p>)}
+
+
+
+     <div className="flex place-content-between">
+        <p className="font-semibold">Rp. {priceAfterDiscount}</p>
+        <button className="flex-end px-3 py-2 bg-orange-600 text-white rounded-full text-sm">BUY NOW</button>
+     </div>
+
+</div>
+}
+
+export default ProductCard;
