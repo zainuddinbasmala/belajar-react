cb39c698f07c67622e5ae278e6b4810fadc50ae1
zainuddinbasmala <zainuddinbasmala@gmail.com>
2022-03-19 16:09:12 +0700

    modify productCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -7,14 +7,16 @@ const ProductCard = (props) => {
      <img src={`images/${props.item.image}`} className="" alt={props.item.name}/>
      {props.item.discountPercent > 0 && (<p className="absolute top-2 left-2 bg-white text-red px-2 py-0.5 rounded-lg text-sm">- {item.discountPercent}%</p>)}</div>
      <h3 className="font-semibold">{props.item.name}</h3>
-     <p>{props.item.description}</p>
-     {props.item.discountPercent > 0 && (
+     <p className="h-16">{props.item.description}</p>   
+
+     <div className="flex place-content-between h-16 items-center">
+        <div className="flex flex-col">
+        {props.item.discountPercent > 0 && (
      <p className="text-xs line-through">Rp. {props.item.originalPrice}</p>)}
-
-
-
-     <div className="flex place-content-between">
         <p className="font-semibold">Rp. {priceAfterDiscount}</p>
+
+        </div>
+     
         <button className="flex-end px-3 py-2 bg-orange-600 text-white rounded-full text-sm">BUY NOW</button>
      </div>
 
